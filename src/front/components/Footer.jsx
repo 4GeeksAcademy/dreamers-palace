6be124d8c70c84a46fd3b492d637779bb2c2ff78
César Chavez <cesar.chavez96@icloud.com>
2025-08-22 00:13:23 +0000

    updated styles on navbar and footer

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,11 +1,7 @@
+import "/workspaces/dreamers-palace/timeline.css"
+
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Check the <a target="_blank" href="https://4geeks.com/docs/start/react-flask-template">template documentation</a> <i className="fa-solid fa-file"></i> for help.
-		</p>
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+	<footer className="py-3 fixed-bottom glass" style={{fontSize: "20px"}}>
+		<p className="text-center text-light mb-0">© Dreamer's Palace</p>
 	</footer>
 );
